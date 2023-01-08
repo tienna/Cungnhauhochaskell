accfc2336a2b2a6bfd40314ec716e25e5e7e3e35
Nguyen Anh Tien <34856010+tienna@users.noreply.github.com>
2023-01-08 16:49:00 +0000

    update

--- Main.hs
+++ Main.hs
@@ -18,7 +18,6 @@ take' n (x:xs) = x : take' (n-1) xs
 
 fruitwA = filter finda ["Tao","Le","Chanh","Dao"]
  where finda  x = 'a' `elem` x
-
 --inlonhon4 x = filter biggerthan4 x
 --  where biggerthan4 x = x>4
 inlonhon4 x = filter (\x -> x>4) x
@@ -27,21 +26,16 @@ lonhon4 x = any biggerthan4 x
   where biggerthan4 x = x>4
 timcar cars= filter (\(_,x) -> x > 0) cars
 dem x = length . filter (== x) $ votes
-
-
-
 uncurry' :: (a ->b ->c) -> (a, b) ->c
 uncurry' f (a,b) = f a b
-
-
 curry' f a b = f (a,b)
--}
-
-
-
 votes :: [String]
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 countVotes :: String -> Int
 
 countVotes color= length $ filter (\x -> x==color) votes
 --countVotes color= length $ filter (==color) votes
+-}
+
+hasUppercase :: [Char] -> Bool
+hasUppercase a = any (\a ->a `elem` ['A'..'Z']) a
