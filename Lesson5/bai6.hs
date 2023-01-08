961854c042cced135e206831dbc48c69fc2fd103
Nguyen Anh Tien <34856010+tienna@users.noreply.github.com>
2023-01-08 16:32:13 +0000

    Upload bai tap lesson5

---
+++ Lesson5/bai6.hs
@@ -0,0 +1,7 @@
+-- Create a one-line function that filters `cars` by brand and then checks if there are any left.
+-- cars = [("Toyota",0), ("Nissan",3), ("Ford",1)]
+-- timcar = filter (\(_,x) -> x > 0) cars
+
+timcar cars= filter (\(_,x) -> x > 0) cars
+
+ 
--- Main.hs
+++ Main.hs
@@ -1,10 +1,47 @@
--- Question 1
--- Lets say you have the nested values defined bellow. How would you get the value of
--- 4 by using only pattern matching in a function?
--- nested :: [([Int], [Int])]
--- nested = [([1,2],[3,4]), ([5,6],[7,8])]
+{-and' :: [Bool] -> Bool
+and' []     = True
+and' (x:xs) = x && and' xs
 
-pick4 ::[([Int], [Int])] -> Int
--- pick4 [([_,_],[_,x]),_] = x
-pick4 [(_,[_,x]),_] = x
+drop' :: Int -> [a] -> [a]
+drop' _ []     = []
+drop' 0 xs     = xs
+drop' n (_:xs) = drop' (n - 1) xs
 
+reserve' :: [a] -> [a]
+reserve' [] = []
+reserve' (a:b) =  reserve' b
+
+take' :: Int -> [a] -> [a]
+take' n _ | n <=0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+fruitwA = filter finda ["Tao","Le","Chanh","Dao"]
+ where finda  x = 'a' `elem` x
+
+--inlonhon4 x = filter biggerthan4 x
+--  where biggerthan4 x = x>4
+inlonhon4 x = filter (\x -> x>4) x
+ 
+lonhon4 x = any biggerthan4 x
+  where biggerthan4 x = x>4
+timcar cars= filter (\(_,x) -> x > 0) cars
+dem x = length . filter (== x) $ votes
+
+
+
+uncurry' :: (a ->b ->c) -> (a, b) ->c
+uncurry' f (a,b) = f a b
+
+
+curry' f a b = f (a,b)
+-}
+
+
+
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+countVotes :: String -> Int
+
+countVotes color= length $ filter (\x -> x==color) votes
+--countVotes color= length $ filter (==color) votes
